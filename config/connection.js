986c140020624d4463a60545dfaf0d9050733a5b
Charles Srstka <support@charlessoft.com>
2018-06-28 19:39:05 -0500

    Add Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const conn = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'SammySquirrel',
-  database: 'burgers_db',
-});
+const conn = (() => {
+  if (process.env.JAWSDB_URL) {
+    return mysql.createConnection(process.env.JAWSDB_URL);
+  }
+
+  return mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'SammySquirrel',
+    database: 'burgers_db',
+  });
+})();
 
 conn.connect((err) => {
   if (err) { throw err; }
